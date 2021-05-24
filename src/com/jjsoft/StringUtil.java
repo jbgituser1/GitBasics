07448e8254431447e694255c047bbaddecb0ce6a
JagadeeshBabu <jb.git.user1@gmail.com>
2021-05-24 22:32:22 +0530

    usage1
    usage1

--- src/com/jjsoft/StringUtil.java
+++ src/com/jjsoft/StringUtil.java
@@ -2,7 +2,7 @@ package com.jjsoft;
 
 public class StringUtil {
 	
-	public boolean isNull(String name) {
+	public static boolean isNull(String name) {
 		if(null == name || "".equals(name)) {
 			return true;
 		}else {
